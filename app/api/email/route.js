a5c53741d1bb49e6f831e68475761ba0a1e29d14
Harsh <kaushikyuvraj134@gmail.com>
2025-08-02 17:48:49 +0530

    add delete email function

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -3,10 +3,27 @@ import EmailModel from "@/lib/models/EmailModel";
 import { NextResponse } from "next/server";
 
 const loadDB = async () => {
-    connectDB();
+    await connectDB();
 }
 
 loadDB();
+
+export async function GET(request){
+    try {
+        const emails = await EmailModel.find({});
+        return NextResponse.json({ success: true, emails });
+    } catch (error) {
+        console.error("GET error:", error);
+        return NextResponse.json({ success: false, message: "Failed to fetch emails" }, { status: 500 });
+    }
+}
+
+export async function DELETE(request){
+    const id = await request.nextUrl.searchParams.get('id');
+    await EmailModel.findByIdAndDelete(id);
+
+    return NextResponse.json({success: true, message: 'Email Deleted'})
+}
 
 export async function POST(request){
     const formData = await request.formData();
